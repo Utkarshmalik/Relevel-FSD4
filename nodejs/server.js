bde9f1c997090b89e8e34dfc62b6cb18e9a0541b
utmalik <utmalik@amazon.com>
2022-09-03 12:43:47 +0530

    CRUD operations

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,48 +1,106 @@
 const express=require("express");
-var morgan = require('morgan')
+const uuid = require("uuid");
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
 
 const app=express();
 
-const db={
-    123:"Utkarsh",
-    567:"Rahul",
-    873:"Shrey"
-}
-
-app.use(morgan('combined'));
+app.use(bodyParser.json())
 
 
-app.use(function(req,res,next){
-    console.log(`New request receieved now : ${Date.now()}`);
-    next();
+let usersDB=[{
+    id:1,
+    name:"Utkarsh",
+    email:"utmalik@amazon.com"
+}]
+
+
+app.get("/users",function(req,res){
+    res.json(usersDB);
 })
 
-app.use(function(req,res,next){
-    const isAuthenticated=true;
+app.post("/users",function(req,res){
 
-    if(!isAuthenticated){
-        res.status(403).send("Invalid request");
-    }else{
-        next();
+    if(!req.body.name || !req.body.email){
+        res.status(400).send({message:"Either name or email is passed empty"});
     }
+
+    const userObj={
+        id:uuid.v4(),
+        name:req.body.name,
+        email:req.body.email
+    };
+
+    usersDB.push(userObj);
+
+    res.status(201).send(userObj);
+})
+
+app.get("/users/:id",function(req,res){
+
+    const id=req.params.id;
+
+    const user=usersDB.find((userElement)=>{
+        return userElement.id==id;
+    })
+
+    if(!user){
+        res.status(404).send({message:"User id is invalid"});
+    }
+    res.json(user);
+})
+
+app.put("/users/:id",function(req,res){
+
+    const id=req.params.id;
+
+    const user=usersDB.find((userElement)=>{
+        return userElement.id==id;
+    })
+    
+    if(!user){
+        res.status(404).send({message:"User id is invalid"});
+    }
+
+    const keysToBeUpdated =Object.keys(req.body);
+
+    keysToBeUpdated.forEach((key)=>{
+        user[key]=req.body[key];
+    })
+
+    res.send(user);
+})
+
+app.delete("/users",function(req,res){
+    usersDB=[];
+
+    res.send({message:"All users deleted successfully"});
 })
 
 
-app.get("/",function(req,res){
-    res.send("Express js backend http endpoint is ready !!!");
-});
+app.delete("/users/:id",function(req,res){
 
-app.get("/name",function(req,res){
-    res.send("Express js backend http endpoint is ready! Name: Utkarsh Malik")
-});
+    const id=req.params.id;
 
+    const user=usersDB.find((userElement)=>{
+        return userElement.id==id;
+    })
+    
+    if(!user){
+        res.status(404).send({message:"User id is invalid"});
+    }
 
-app.get(/^\/users\/(\d+)$/,function (req,res){
-
-    const userId=req.params[0];
-   const UserDetails=db[userId];
-   res.send(`Hello from ${UserDetails}`);
+    usersDB = usersDB.filter((user)=>{
+        return user.id!=id;
+    })
+    
+    res.send({message:`user with userId: ${id} deleted successfully`});
 })
+
+
+
+
 
 app.listen(8000,()=>{
     console.log("Application is listening on port 8000");
